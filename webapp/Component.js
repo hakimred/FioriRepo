cc571c73526452afb4081118e5e2cd8107468cef
Abdul Hakim Sulaiman Batcha <abdul.hakim@accenture.com>
2024-03-19 23:26:43 +0000

    myapp

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.ats.tproc.travelprocessorah.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
